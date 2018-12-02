bd4e66d471f69cb994600188f29ef6a5be00ffea
NeusRB <neuzeta@gmail.com>
2018-12-02 11:22:43 +0100

    HornsMonsterData file

---
+++ Assets/Scripts/HornsMonsterData.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HornsMonsterData : MonoBehaviour {
+
+    [System.Serializable]
+    public class HornsMonsterLevel
+    {
+        public int cost;
+        public GameObject visualization;
+    }
+
+    public List<HornsMonsterLevel> levels;
+
+    private HornsMonsterLevel currentLevel;
+    public HornsMonsterLevel CurrentLevel
+    {
+        get
+        {
+            return currentLevel;
+        }
+        set
+        {
+            currentLevel = value;
+            int currentLevelIndex = levels.IndexOf(currentLevel);
+
+            GameObject levelVisualization = levels[currentLevelIndex].visualization;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levelVisualization != null)
+                {
+                    if (i == currentLevelIndex)
+                    {
+                        levels[i].visualization.SetActive(true);
+                    }
+                    else
+                    {
+                        levels[i].visualization.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        CurrentLevel = levels[0];
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
